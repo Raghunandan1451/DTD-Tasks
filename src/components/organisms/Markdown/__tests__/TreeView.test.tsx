2affb37ec3e6a89b8b2392c98969b4361fba0f14
Raghunandan <raghunandansharma615@gmail.com>
2025-04-06 00:15:31 +0530

    chore: Test folders renamed

--- src/components/organisms/Markdown/__tests__/TreeView.test.tsx
+++ src/components/organisms/Markdown/__tests__/TreeView.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import TreeView from '../../../organisms/Markdown/TreeView';
+import TreeView from '../TreeView';
 import { describe, it, expect, vi } from 'vitest';
 import '@testing-library/jest-dom';
 
