fade80f49327033c3aff7ef549d2634bf065b032
Raghunandan1451 <raghunandansharma615@gmail.com>
2025-07-13 11:49:28 +0530

    chore: Move slice files and updated vite and tsconfig files

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		tsconfigPaths({
+			projects: ["./tsconfig.test.json"],
+		}),
+	],
 	build: {
 		emptyOutDir: true,
 	},
 	optimizeDeps: {
 		esbuildOptions: {
 			supported: {
-				'top-level-await': true,
+				"top-level-await": true,
 			},
 		},
 	},
-	resolve: {
-		alias: {
-			'@src': '/src',
-		},
+	test: {
+		globals: true,
+		environment: "jsdom",
 	},
 });
